1a9b71e11faf9ab45fe3c710cec5f704d413e3c4
shut_uper <ttatta3adpot@gmail.com>
2021-10-07 01:31:18 +0300

    lab3 release (100%)

---
+++ js/main3.js
@@ -0,0 +1,20 @@
+function changeClass() {
+    if (document.getElementById("changeArrow").classList.contains('bi-arrow-down-circle')) {
+        document.getElementById("changeArrow").classList.remove('bi-arrow-down-circle');
+        document.getElementById("changeArrow").classList.add('bi-arrow-up-circle');
+    } else {
+        document.getElementById("changeArrow").classList.remove('bi-arrow-up-circle');
+        document.getElementById("changeArrow").classList.add('bi-arrow-down-circle');
+    }
+}
+
+function changeMain(id) {
+    const allIds = ['default', 'team_photo', 'macket-view', 'html-macket', 'floaters', 'tables', 'theme', 'conclusion'];
+    for (let i = 0; i < allIds.length; i++) {
+        if (!document.getElementById(allIds[i]).classList.contains('d-none')) {
+            document.getElementById(allIds[i]).classList.add('d-none');
+            break;
+        }
+    }
+    document.getElementById(id.value).classList.remove('d-none');
+}
