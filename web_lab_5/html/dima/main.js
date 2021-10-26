b655abb2b5be80f16bec5f1b8899576353fa6730
Kotvtapkax228 <tereschenko.dmytro@gmail.com>
2021-10-27 01:08:52 +0300

    dima done laba5

---
+++ web_lab_5/html/dima/main.js
@@ -0,0 +1,31 @@
+
+$('.js-button-campaign').click(function() {
+
+    $('.js-overlay-campaign').fadeIn();
+    $('.js-overlay-campaign').addClass('disabled');
+});
+
+$('.js-close-campaign').click(function() {
+    $('.js-overlay-campaign').fadeOut();
+
+});
+
+$(document).mouseup(function (e) {
+    var popup = $('.js-popup-campaign');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-campaign').fadeOut();
+
+    }
+});
+
+$(window).on('load', function () {
+    setTimeout(function(){
+        if($('.js-overlay-campaign').hasClass('disabled')) {
+            return false;
+        } else {
+
+            $(".js-overlay-campaign").fadeIn();
+        }
+    }, 5000);
+});
+
