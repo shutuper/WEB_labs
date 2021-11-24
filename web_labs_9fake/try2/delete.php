31b5cc4def4a50b614d468e132d2b70d80901859
Kotvtapkax228 <tereschenko.dmytro@gmail.com>
2021-11-25 00:17:45 +0200

    dimona dana

--- web_labs_9fake/try2/delete.php
+++ web_labs_9fake/try2/delete.php
@@ -4,7 +4,6 @@ $pdo = pdo_connect_mysql();
 $msg = '';
 // Check that the image ID exists
 if (isset($_GET['id'])) {
-    // Select the record that is going to be deleted
     $stmt = $pdo->prepare('SELECT * FROM images WHERE id = ?');
     $stmt->execute([ $_GET['id'] ]);
     $image = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -14,14 +13,11 @@ if (isset($_GET['id'])) {
     // Make sure the user confirms before deletion
     if (isset($_GET['confirm'])) {
         if ($_GET['confirm'] == 'yes') {
-            // User clicked the "Yes" button, delete file & delete record
             unlink($image['filepath']);
             $stmt = $pdo->prepare('DELETE FROM images WHERE id = ?');
             $stmt->execute([ $_GET['id'] ]);
-            // Output msg
             $msg = 'You have deleted the image!';
         } else {
-            // User clicked the "No" button, redirect them back to the home/index page
             header('Location: index.php');
             exit;
         }
--- web_labs_9fake/try2/functions.php
+++ web_labs_9fake/try2/functions.php
@@ -6,14 +6,12 @@ function pdo_connect_mysql() {
     $DATABASE_PASS = 'dima5787';
     $DATABASE_NAME = 'photogallery';
     try {
-        // Connect to MySQL using the PDO extension
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
-        // If there is an error with the connection, stop the script and output the error.
         exit('Failed to connect to database!');
     }
 }
-// Template header; feel free to customize it, but do not indent the PHP code or it will throw an error
+
 function template_header($title) {
     echo <<<EOT
 <!DOCTYPE html>
@@ -27,13 +25,13 @@ function template_header($title) {
 	<body>
     <nav class="navtop">
     	<div>
-    		<h1>Gallery System</h1>
+    		<h1>Галерея Hokage</h1>
             <a href="index.php"><i class="fas fa-image"></i>Gallery</a>
     	</div>
     </nav>
 EOT;
 }
-// Template footer
+
 function template_footer() {
     echo <<<EOT
     </body>
